480d6fe651ca5d33ec5c29e79021ff7f79792c10
Nikita Zhulev <Nikita_Zhulev@epam.com>
2022-01-31 18:49:14 +0300

    add scss check

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     extends: ["stylelint-config-standard",
               "stylelint-config-rational-order",
-              "stylelint-prettier/recommended"],
+              "stylelint-prettier/recommended",
+              "stylelint-config-standard-scss"],
     plugins: ["stylelint-order", "stylelint-scss"],
     rules: {
       // Дополнительные правила
