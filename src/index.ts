11f0a522673f49f9949158ebdb6eb8bae69bf1b2
Nikita Zhulev <Nikita_Zhulev@epam.com>
2022-02-04 16:08:01 +0300

    async/await version

--- src/index.ts
+++ src/index.ts
@@ -1,123 +1,124 @@
-// @ts-ignore
-// eslint-disable
-
 import './style.scss';
 
-function loadDefault() {
-    let albumNumber: number = 0;
+// albums: https://jsonplaceholder.typicode.com/albums/
+// photos: https://jsonplaceholder.typicode.com/photos/
 
-    const requestURL: string =
-        'http://jsonplaceholder.typicode.com/albums?_limit=8';
+const albumsUrl: string = 'https://jsonplaceholder.typicode.com/albums/';
+const photosUrl: string = 'https://jsonplaceholder.typicode.com/photos/';
+const defaultAlbumsNumberOnPage: number = 8;
 
-    const thumbnailURL: string = 'http://jsonplaceholder.typicode.com/photos';
+let clickedElementId: string = '';
+const albumsArr: Array<{
+    userId: number;
+    id: number;
+    title: string;
+}> = [];
+const photosArr: Array<{
+    albumId: number;
+    id: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
+}> = [];
 
-    fetch(requestURL, {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            return data;
-        })
-        .then((collection) => {
-            for (let i = 0; i < 8; i++) {
-                const album = document.createElement('div');
-                const heading = document.createElement('h3');
-                const id = document.createElement('p');
-
-                album.classList.add('album');
-                album.id = (i + 1).toString();
-                heading.classList.add('album__heading');
-                id.classList.add('album__ID');
-
-                heading.innerHTML = collection[albumNumber].title;
-                id.innerHTML = collection[albumNumber].id;
-
-                document.body.appendChild(album);
-                album.appendChild(heading);
-                album.appendChild(id);
-
-                albumNumber += 1;
-            }
-            const thumbnailURLs: Promise<
-                [
-                    {
-                        albumId: number;
-                        id: number;
-                        title: string;
-                        url: string;
-                        thumbnailUrl: string;
-                    }
-                ]
-            >[] = collection.map((album) =>
-                fetch(
-                    `http://jsonplaceholder.typicode.com/photos?albumId=${album.id}&_limit=1`
-                ).then((data) => data.json())
-            );
-            Promise.allSettled(thumbnailURLs).then((results) => results);
-        })
-        .then(() =>
-            fetch(thumbnailURL, {
-                method: 'GET',
-                headers: {
-                    'Content-type': 'photos/json; charset=UTF-8',
-                },
-            })
-        )
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            return data;
-        })
-        .then((photos) => {
-            const albums = document.querySelectorAll('.album');
-            albums.forEach((album, index) => {
-                const albumCover = document.createElement('img');
-                albumCover.classList.add('album__first-image');
-                albumCover.setAttribute('src', photos[index].thumbnailUrl);
-                album.appendChild(albumCover);
-            });
-        });
+function setThumbnailSource(albumId: number) {
+    const thumbnailsPath: string[] = [];
+    photosArr.filter((image) => {
+        if (image.albumId === albumId + 1) {
+            return thumbnailsPath.push(image.thumbnailUrl);
+        }
+    });
+    return thumbnailsPath[0];
 }
 
-document.addEventListener('DOMContentLoaded', loadDefault);
+async function fetchAlbums() {
+    try {
+        const getAlbums = await fetch(albumsUrl);
+        const albums = await getAlbums.json();
+        for (let i = 0; i < albums.length; i++) {
+            albumsArr.push(albums[i]);
+        }
+        return albums;
+    } catch (err) {
+        console.log('There was an error', err);
+    }
+}
 
+async function fetchPhotos() {
+    try {
+        const getPhotos = await fetch(photosUrl);
+        const photos = await getPhotos.json();
+        // console.log(albums);
+        for (let i = 0; i < photos.length; i++) {
+            photosArr.push(photos[i]);
+        }
+        // console.log(albumsArr);
+        return photos;
+    } catch (err) {
+        console.log('There was an error', err);
+    }
+}
+
+async function createElements() {
+    try {
+        await fetchAlbums();
+        await fetchPhotos();
+        for (let i = 0; i < defaultAlbumsNumberOnPage; i++) {
+            const album = document.createElement('div');
+            const heading = document.createElement('h3');
+            const id = document.createElement('p');
+            const thumbnail = document.createElement('img');
+
+            album.classList.add('album');
+            album.id = (i + 1).toString();
+            heading.classList.add('album__heading');
+            id.classList.add('album__ID');
+            thumbnail.classList.add('album__first-image');
+
+            heading.innerHTML = albumsArr[i].title;
+            // id.innerHTML = albumsArr[i].id;
+
+            thumbnail.setAttribute('src', setThumbnailSource(i));
+
+            document.body.appendChild(album);
+            album.appendChild(heading);
+            album.appendChild(id);
+            album.appendChild(thumbnail);
+        }
+    } catch (err) {
+        console.log('There was an error', err);
+    }
+}
+
+function loadDefault() {
+    createElements();
+}
+document.addEventListener('DOMContentLoaded', loadDefault);
 document.addEventListener('click', (event) => {
     if ((event.target as HTMLElement).classList.contains('album')) {
+        clickedElementId = (event.target as HTMLElement).id;
         document.body.innerHTML = '';
-        const photoAlbumURL: string = `https://jsonplaceholder.typicode.com/photos/?albumId=${
-            (event.target as HTMLElement).id
-        }`;
-        fetch(photoAlbumURL, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'strangeTest/json; charset=UTF-8',
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                return data;
-            })
-            .then((gallery) => {
-                const galleryWrapper = document.createElement('div');
-                const returnBtn = document.createElement('button');
-                galleryWrapper.classList.add('gallery__Wrapper');
-                returnBtn.classList.add('gallery__btn-back');
-                returnBtn.innerHTML = 'return';
-                document.body.appendChild(galleryWrapper);
-                galleryWrapper.appendChild(returnBtn);
+        const galleryWrapper = document.createElement('div');
+        const returnBtn = document.createElement('button');
 
-                for (let i = 0; i < gallery.length; i++) {
-                    const image = document.createElement('img');
-                    image.classList.add('gallery__image');
-                    image.setAttribute('src', gallery[i].url);
-                    galleryWrapper.appendChild(image);
-                }
-            });
+        galleryWrapper.classList.add('gallery__Wrapper');
+        returnBtn.classList.add('gallery__btn-back');
+        returnBtn.innerHTML = 'return';
+
+        document.body.appendChild(galleryWrapper);
+        galleryWrapper.appendChild(returnBtn);
+
+        const currentGallery = photosArr.filter((image) => {
+            if (image.albumId.toString() === clickedElementId) {
+                return image;
+            }
+        });
+        for (let i = 0; i < currentGallery.length; i++) {
+            const image = document.createElement('img');
+            image.classList.add('gallery__image');
+            image.setAttribute('src', currentGallery[i].url);
+            galleryWrapper.appendChild(image);
+        }
     }
 });
 
