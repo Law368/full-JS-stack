a8afcd3b3b31144232ffefec2410e0d30c226697
Nikita Zhulev <Nikita_Zhulev@epam.com>
2022-02-18 16:54:17 +0300

    delete renderPage function

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,10 @@ import {fetchAlbumsInfo} from './modules/fetchAlbumsInfo';
 
 const screenType = localStorage.getItem('screenType');
 
-async function renderPage() {
-    if (screenType === ScreenType.albums) {
-        renderAlbums();
-    } else if (screenType === ScreenType.gallery) {
-        renderGallery();
-    }
-}
 fetchAlbumsInfo();
-renderPage();
+
+if (screenType === ScreenType.albums) {
+    renderAlbums();
+} else {
+    renderGallery();
+}
